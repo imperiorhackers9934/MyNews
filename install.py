469d95cec6c7642297c4941d4ce13f463c77c75e
Imperior Hackers <58589595+imperiorhackers9934@users.noreply.github.com>
2021-09-12 12:00:12 +0530

    Setup File

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import time
+os.system("clear")
+os.system("python -m pip install –upgrade pip")
+os.system("pip install pyttsx3")
+print("Your News Script has been Installed")
+print("Please Wait.....")
+time.sleep(3)
+os.system("News.py")
